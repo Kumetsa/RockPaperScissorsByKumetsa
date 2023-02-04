62261983cfe4e824d07fbaca43514a2f781b495e
Kumetsa <i.e.ivanov1989@gmail.com>
2023-02-04 17:01:02 +0200

    Added validation
    Validation for input added.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -5,7 +5,8 @@ from colorama import Style
 
 colorama_init()
 
-# Score counter
+player_name = input("Enter Player Name:") or "Unknown player"
+
 player_score = 0
 computer_score = 0
 draws = 0
@@ -17,7 +18,7 @@ while True:
 
     player_move = input("Choose [r]ock, [p]aper or [s]cissors:")
 
-    # Formatting player input
+    # Validating & Formatting player input
     if player_move == "r":
         player_move = rock
     elif player_move == "p":
@@ -25,7 +26,8 @@ while True:
     elif player_move == "s":
         player_move = scissors
     else:
-        raise SystemExit("Invalid input. Try again...")
+        print("Invalid input. Try again...")
+        continue
 
     computer_random_number = random.randint(1, 3)
 
@@ -51,7 +53,7 @@ while True:
         computer_score += 1
         print(f"{Fore.RED}You lose!{Style.RESET_ALL}")
 
-    print(f"Current score: Player:[{player_score}] | Draws:[{draws}] | Computer:[{computer_score}]")
+    print(f"Current score: {player_name}:[{player_score}] | Draws:[{draws}] | Computer:[{computer_score}]")
     # Logic to restart the game
     while True:
         answer = str(input("Type [yes] to play again, or [no] to quit:"))
