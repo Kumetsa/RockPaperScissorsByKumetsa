334205080f12786d4de7a7d642615af0839e8861
Kumetsa <i.e.ivanov1989@gmail.com>
2023-02-04 15:20:57 +0200

    Scoring
    Added scoring system to the game !

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -5,6 +5,10 @@ from colorama import Style
 
 colorama_init()
 
+# Score counter
+player_score = 0
+computer_score = 0
+draws = 0
 
 while True:
     rock = "Rock"
@@ -36,14 +40,18 @@ while True:
 
     print(f"The computer chose {computer_move}")
 
-    # Logic to determine the winner
+    # Logic to determine the winner and count score
     if player_move == rock and computer_move == scissors or player_move == paper and computer_move == rock or player_move == scissors and computer_move == paper:
+        player_score += 1
         print(f"{Fore.GREEN}You win!{Style.RESET_ALL}")
     elif player_move == computer_move:
+        draws += 1
         print(f"{Fore.YELLOW}Draw!{Style.RESET_ALL}")
     else:
+        computer_score += 1
         print(f"{Fore.RED}You lose!{Style.RESET_ALL}")
 
+    print(f"Current score: Player:[{player_score}] | Draws:[{draws}] | Computer:[{computer_score}]")
     # Logic to restart the game
     while True:
         answer = str(input("Type [yes] to play again, or [no] to quit:"))
@@ -53,5 +61,6 @@ while True:
     if answer == "yes":
         continue
     else:
+        print(f"Final score: Player:[{player_score}] | Draws:[{draws}] | Computer:[{computer_score}] ")
         print("Goodbye!")
         break
